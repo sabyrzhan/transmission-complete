c8541ffdba53f7c55a65e3b710fa4b3f1fb435ca
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-03-15 23:38:10 +0200

    - Seperate logics to functions - delete venv before each run

--- move.py
+++ move.py
@@ -5,40 +5,64 @@ import shutil
 
 download_dir = 'TORRENTS_FOLDER'
 move_to = 'TARGET_FOLDER'
-makedirs(move_to, exist_ok=True)
-all_dirs = set()
-for f in listdir(download_dir):
-    if f == '_completed':
-        continue
-    if isdir(download_dir + '/' + f):
-        all_dirs.add(f)
+
+def validate_dirs():
+    if download_dir == 'TORRENTS_FOLDER':
+        print('ERROR: "download_dir" folder was not set')
+        return False
+    
+    if move_to == 'TARGET_FOLDER':
+        print('ERROR: "move_to" folder was not set')
+        return False
+    
+    return True
+
+def prepare():
+    makedirs(move_to, exist_ok=True)
+    all_dirs = set()
+    for f in listdir(download_dir):
+        if f == '_completed':
+            continue
+        if isdir(download_dir + '/' + f):
+            all_dirs.add(f)
 
 
-c = Client()
-r = c.get_torrents()
-downloading = set()
-for i in r:
-    if i.status != 'seeding':
-        downloading.add(i.name)
+    c = Client()
+    r = c.get_torrents()
+    downloading = set()
+    for i in r:
+        if i.status != 'seeding':
+            downloading.add(i.name)
 
-completed = all_dirs.difference(downloading)
+    completed = all_dirs.difference(downloading)
 
-if len(completed) == 0:
-  print('No completed downloads found')
-  exit()
+    return completed
 
-print('Following dirs will be moved: to "%s"' % move_to)
-for d in completed:
-    print("  - %s" % d)
-print()
-print("Do you confirm? [y/n]")
-confirm = str(input())
-if confirm == 'y':
-    for c in completed:
-        spath = download_dir + '/' + c
-        tpath = move_to + '/' + c
-        shutil.move(spath, tpath)
-else:
-    print('Canceled!')
+def move_files(completed):
+    if len(completed) == 0:
+        print('ERROR: No completed downloads found')
+        return
 
-print('Finished!')
+    print('Following dirs will be moved: to "%s"' % move_to)
+    i = 0
+    for d in completed:
+        print("  %2d) %s" % (i + 1, d))
+        i+=1
+    print()
+    print("Do you confirm? [y/n]")
+    confirm = str(input())
+    if confirm == 'y':
+        for c in completed:
+            spath = download_dir + '/' + c
+            tpath = move_to + '/' + c
+            shutil.move(spath, tpath)
+            print('Finished!')
+    else:
+        print('Canceled!')
+
+
+if __name__ == '__main__':
+    is_valid = validate_dirs()
+    if is_valid:
+        completed = prepare()
+        move_files(completed)
--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+if [ -d $PWD/venv ]; then
+    rm -rf $PWD/venv
+fi
+
 python3 -m venv ./venv
 . ./venv/bin/activate
 pip install -r requirements.txt
 python move.py
+deactivate
